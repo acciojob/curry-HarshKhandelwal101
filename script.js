0e1c99f864869cae14dacbfb53a37687ebb19c25
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-07 18:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function curry(callback) {
-  
+  return function curried(...args) {
+    if (args.length >= callback.length) {
+      return callback.apply(this, args);
+    } else {
+      return function(...moreArgs) {
+        return curried.apply(this, args.concat(moreArgs));
+      };
+    }
+  };
 }
-
-module.export=curry
